51293a87a274dcbe145c83b0c352cc0ea57f3821
J3cob25 <dobes.jakub25@gmail.com>
2025-01-27 00:20:05 +0100

    Add files via upload

---
+++ src/test/java/Swi224ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.pesa.swi224;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swi224ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
